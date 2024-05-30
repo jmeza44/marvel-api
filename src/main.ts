6dd95f3cb0bbdabfd04fb1c4ceeea3507b4ef914
Jaime Meza <jaimealbertomeza@hotmail.com>
2024-05-30 03:57:41 -0500

    Add fix for connection timeout

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { hostname } from 'os';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -18,7 +19,11 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(process.env.PORT || 3000);
+  // await app.listen(process.env.PORT || 3000, , () => );
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0', () => {
+    console.log(`Listening on Port ${port} - Host ${hostname}`);
+  });
 }
 
 bootstrap();
