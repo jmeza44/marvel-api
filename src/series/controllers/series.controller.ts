7047a83dca8a1259d490cae4486117ae5178c6f5
Jaime Meza <jaimealbertomeza@hotmail.com>
2024-05-31 01:00:15 -0500

    Endpoint to retrieve series details

--- src/series/controllers/series.controller.ts
+++ src/series/controllers/series.controller.ts
@@ -1,8 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Serie } from 'src/shared/models/marvel-api/core/marvel-api-serie.model';
+import { SeriesService } from '../services/series.service';
+import { Observable } from 'rxjs';
 
 @ApiTags('Series')
 @Controller('series')
 export class SeriesController {
-  constructor() {}
+  constructor(private seriesService: SeriesService) {}
+
+  @Get(':serieId')
+  getSerieById(@Param('serieId') serieId: number): Observable<Serie> {
+    return this.seriesService.getSerieById(serieId);
+  }
 }
